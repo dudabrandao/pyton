d9a5d9c61ba9951f268de158553bade769d4bb38
Maria Eduarda <124930672+dudabrandao@users.noreply.github.com>
2023-06-16 09:26:30 -0300

    Add files via upload

---
+++ tupla/exercicio1.py
@@ -0,0 +1,3 @@
+numeros = 4,5,8,9,2,3,1,0
+numeroscrescente = (sorted(numeros))
+print(numeroscrescente)
---
+++ tupla/exercicio4.py
@@ -0,0 +1,4 @@
+contato1= ("letícia", 99123-4567, "leticia@gmail.com")
+contato2= ("emy", 99321-7654, "emy@gmail.com")
+contato3= ("heloá", 99746-8932, "heloa@gmail.com")
+print(contato1, contato2, contato3)
